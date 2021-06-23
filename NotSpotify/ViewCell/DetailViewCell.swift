4f48ba5f347ea024c458ed747bccdf3a3fa9cd20
Matheus Homrich <matidasilva34@gmail.com>
2021-06-23 16:43:59 -0300

    na

---
+++ NotSpotify/ViewCell/DetailViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewCell.swift
+//  NotSpotify
+//
+//  Created by Matheus Homrich on 23/06/21.
+//
+
+import UIKit
+
+class DetailViewCell: UITableViewCell {
+    
+    @IBOutlet weak var albumCover: UIImageView!
+    @IBOutlet weak var albumName: UILabel!
+    @IBOutlet weak var albumArtist: UILabel!
+    @IBOutlet weak var numberOfSongs: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var albumDescription: UILabel!
+    
+    
+}
